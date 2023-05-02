e7bef82aa87f477a33cb0232aa40f9876be6e529
cmrd-k <phixix@hotmail.com>
2023-05-02 16:34:27 +0200

    merges branch retina to dev to replace Haarcasscade with RetinaFace, tracks Frame Processing Rate, total processing Time, Progress in % and total Processing Time, also Updates gitignore

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
 import cv2
-import pathlib
+from retinaface import RetinaFace
 import numpy as np
-
-# defines path to Facerecognition classifier and assigns filter to clf
-cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
-clf = cv2.CascadeClassifier(str(cascade_path))
+import time
 
 #defines source for video, 0 for webcam, replace 0 with filepath to use saved videos
-video = cv2.VideoCapture("./videos/videoplayback.mp4")
+video = cv2.VideoCapture("./videos/video.mp4")
+height = int(video.get(4)) #gets video height
+width = int(video.get(3)) #gets video widht
+frameCount = int(video.get(7))
+framesProcessed = 0
+print(f'total frames: {frameCount}')
+export = cv2.VideoWriter('./videos/output1.mp4', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 20.0, (width,height))
 
-while True:
-    width = int(video.get(3)) #gets video widht
-    height = int(video.get(4)) #gets video height
-    mask = np.zeros((height, width, 3), dtype=np.uint8)
-    _, frame = video.read()
-    blurred = cv2.GaussianBlur(frame, (99,99),0)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = clf.detectMultiScale(
-        gray,
-        scaleFactor = 1.1,
-        minNeighbors = 5,
-        minSize =(30,30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
 
-    for (x, y, width, height) in faces:
-        mask = cv2.rectangle(mask, (x,y), (x+width, y+height), (255,255,255), -1)
-    out = np.where(mask == np.array([255, 255, 255]), blurred, frame)
-
-    cv2.imshow("Faces", gray)
-    if cv2.waitKey(1) == ord("q"):
+while(video.isOpened()):
+    emptyMask = np.zeros((height, width, 1), dtype=np.uint8)  # creates black(0)->white(255) channel
+    ret, frame = video.read()
+    if ret:
+        blurred = cv2.GaussianBlur(frame, (99,99),0)
+        faces = RetinaFace.detect_faces(frame)
+        # print(faces)
+        try: #try catch added bc for loop throws error when no faces in frame
+            for key in faces.keys():
+                face = faces[key]
+                facial_area = face["facial_area"]
+                mask = cv2.rectangle(emptyMask, (facial_area[2], facial_area[3]), (facial_area[0], facial_area[1]), (255), -1)
+        except Exception as e:
+            print(e)
+        out = np.where(mask == np.array([255]), blurred, frame)
+        export.write(out)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+        framesProcessed += 1
+        print(f'framesprocessed: {framesProcessed}/{frameCount} == {framesProcessed / frameCount * 100}%')
+    else:
         break
 
+processTime = time.process_time()
+processTimeM = processTime/60
+if processTimeM>= 1:
+    print(f'{processTimeM} minutes to process')
+else:
+    print(f'{processTime} seconds to process')
+
+print(f'Which equates to a Frame processing Rate of {frameCount/processTime} Frames per second')
 video.release()
+export.release()
 cv2.destroyAllWindows()
